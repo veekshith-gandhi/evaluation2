f6e988c98bcbed21ae900b7e1228577314d30940
veekshith gandhi <veekshithgandhi@gmail.com>
2021-12-29 22:32:54 +0530

    store

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -7,6 +7,7 @@ import Typography from "@mui/material/Typography";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useSelector } from "react-redux";
 import { shallowEqual } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   const cart = useSelector((state) => state.cart, shallowEqual);
@@ -18,8 +19,10 @@ export default function Header() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Products
           </Typography>
-          <ShoppingCartIcon />
-          <button>{cart?.count || 0}</button>
+          <Link to="/cart">
+            <ShoppingCartIcon />
+            <button>{cart?.count || 0}</button>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
